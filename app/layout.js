9f9cd44fc7a872a8886dc1518037e0c33c37758e
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-07-24 00:08:05 +0900

    configs added

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,12 @@
 import { Outfit, Ovo } from "next/font/google";
 import "./globals.css";
 
-const Outfit = Outfit({
+const outfit = Outfit({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
 });
 
-const Ovo = Ovo({
+const ovo = Ovo({
   subsets: ["latin"],
   weight: ["400"],
 });
@@ -19,7 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${outfit.className} ${ovo.className} antialiased`}>{children}</body>
     </html>
   );
 }
