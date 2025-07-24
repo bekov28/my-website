20bb74620718a3f560a961b45a34a3b6e916fbc2
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-07-25 01:11:05 +0900

    tailwind congig added

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -9,27 +9,41 @@ const Navbar = () => {
         <a href="#top">
           <Image src={assets.logo} className="w-28 cursor-pointer mr-14" alt="" />
         </a>
+        <a href="#top" className="w-28 cursor-pointer mr-14"></a>
 
-        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3">
+        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50">
           <li>
-            <a href="#top">Home</a>
+            <a className="font-Ovo" href="#top">
+              Home
+            </a>
           </li>
           <li>
-            <a href="#about">About me</a>
+            <a className="font-Ovo" href="#about">
+              About me
+            </a>
           </li>
           <li>
-            <a href="#services">Services</a>
+            <a className="font-Ovo" href="#services">
+              Services
+            </a>
           </li>
           <li>
-            <a href="#work">My work</a>
+            <a className="font-Ovo" href="#work">
+              My work
+            </a>
           </li>
           <li>
-            <a href="#contact">Contact me</a>
+            <a className="font-Ovo" href="#contact">
+              Contact me
+            </a>
           </li>
         </ul>
 
         <div>
-          <a href="#contact">
+          <a
+            href="#contact"
+            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4"
+          >
             Contact <Image src={assets.arrow_icon} className="w-3" alt="" />
           </a>
         </div>
