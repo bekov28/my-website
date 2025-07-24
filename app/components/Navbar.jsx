9960cccb122dd370eab4542a613831db938c76d3
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-07-24 23:13:56 +0900

    navbar fixes

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,12 +5,12 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <nav>
-        <a href="">
-          <Image src={assets.logo} className="w-28 cursor-pointer mr-14" />
+      <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
+        <a href="#top">
+          <Image src={assets.logo} className="w-28 cursor-pointer mr-14" alt="" />
         </a>
 
-        <ul>
+        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3">
           <li>
             <a href="#top">Home</a>
           </li>
@@ -30,7 +30,7 @@ const Navbar = () => {
 
         <div>
           <a href="contact">
-            Contact <Image src={assets.arrow_icon} className="w-3" />
+            Contact <Image src={assets.arrow_icon} className="w-3" alt="" />
           </a>
         </div>
       </nav>
--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
-import Navbar from "./components/Navbar";
+"use client"; //This file and its imports (like Navbar) will render on the client
+import Navbar from "./components/Navbar"; //Navbar will inherit client-side behavior
 
 export default function Home() {
   return (
