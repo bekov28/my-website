b33a25ff26a6688979c994e6ea20318787dda643
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-07-26 01:10:17 +0900

    x button added

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -43,13 +43,52 @@ const Navbar = () => {
           </li>
         </ul>
 
-        <div>
+        <div className="flex items-center gap-4">
+          <button>
+            <Image src={assets.moon_icon} alt="" className="w-6" />
+          </button>
           <a
             href="#contact"
             className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4"
           >
             Contact <Image src={assets.arrow_icon} className="w-3" alt="" />
           </a>
+          <button className="block md:hidden ml-2">
+            <Image src={assets.menu_black} alt="" className="w-6" />
+          </button>
+
+          {/*------mobile menu--------*/}
+          <ul className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-0 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500">
+            <div>
+              <Image src={assets.close_black} alt="" className="w-5" cursor-pointer />
+            </div>
+
+            <li>
+              <a className="font-Ovo" href="#top">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="font-Ovo" href="#about">
+                About me
+              </a>
+            </li>
+            <li>
+              <a className="font-Ovo" href="#services">
+                Services
+              </a>
+            </li>
+            <li>
+              <a className="font-Ovo" href="#work">
+                My work
+              </a>
+            </li>
+            <li>
+              <a className="font-Ovo" href="#contact">
+                Contact me
+              </a>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
