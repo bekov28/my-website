ac5d4cc025238d172728167a4bae42d0a4e80130
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-07-26 00:48:21 +0900

    navbar logo updated

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,10 +5,15 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
+      <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
+        <Image src={assets.header_bg_color} alt="" className="w-full" />
+      </div>
+
       <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
-        <a href="#top">
-          <Image src={assets.logo} className="w-28 cursor-pointer mr-14" alt="" />
-        </a>
+        <div className="flex items-center">
+          <h1 className="font-semibold text-3xl">Berd</h1>
+          <span className="inline-block h-2 w-2 rounded-full bg-red-500 mt-4 ml-0.5"></span>
+        </div>
 
         <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50">
           <li>
