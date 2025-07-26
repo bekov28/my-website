74486fc587dd25640e4db482498c2019e0cde319
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-07-26 20:02:27 +0900

    header added

---
+++ app/components/Header.jsx
@@ -0,0 +1,41 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <Image src={assets.profile_img} alt="" className="rounded-full w-32" />
+      </div>
+      <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo">
+        Hi! I'm Berdiyor Orzikulov <Image src={assets.hand_icon} alt="" className=" w-32" />
+      </h3>
+      <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo">
+        Full Stack Web Developer based in Seoul, Korea
+      </h1>
+      <p className="max-w-2xl mx-auto font-Ovo">
+        I am a Full stack engineer from Uzbekistan with around 1 year of experience in multiple
+        Korean companies.
+      </p>
+      <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
+        <a
+          href="#contact"
+          className="px-10 py-3 border border-white rounded-full bg-black  text-white flex items-center gap-2"
+        >
+          contact me <Image src={assets.right_arrow_white} alt="" className="w-4" />
+        </a>
+
+        <a
+          href="/sample-resume.pdf"
+          download
+          className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2"
+        >
+          my resume <Image src={assets.download_icon} alt="" className="w-4" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
