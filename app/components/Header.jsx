176a66d0b2dfa1ea586e19a1271ed1441bac3ecc
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-07-26 22:53:46 +0900

    header updated

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,18 +4,18 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>
+    <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center ga-4">
       <div>
         <Image src={assets.profile_img} alt="" className="rounded-full w-32" />
       </div>
-      <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo">
-        Hi! I'm Berdiyor Orzikulov <Image src={assets.hand_icon} alt="" className=" w-32" />
+      <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-5 font-Ovo">
+        Hi! I'm Berdiyor Orzikulov <Image src={assets.hand_icon} alt="" className=" w-6" />
       </h3>
       <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo">
         Full Stack Web Developer based in Seoul, Korea
       </h1>
       <p className="max-w-2xl mx-auto font-Ovo">
-        I am a Full stack engineer from Uzbekistan with around 1 year of experience in multiple
+        I am a full stack engineer from Uzbekistan with around 1 year of experience in multiple
         Korean companies.
       </p>
       <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
