58d9f2e6625498c5641f29d705b50c58d8900817
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-07-27 19:06:38 +0900

    about section edited

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,4 +1,4 @@
-import { assets } from "@/assets/assets";
+import { assets, infoList } from "@/assets/assets";
 import Image from "next/image";
 import React from "react";
 
@@ -21,6 +21,20 @@ const About = () => {
             practices, I build responsive, performant, and accessible websites. Let's create
             something amazing together!
           </p>
+
+          <ul className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl">
+            {infoList.map(({ icon, iconDark, title, description }, index) => (
+              <li
+                className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover
+                hover:-translate-y-1 duration-500"
+                key={index}
+              >
+                <Image src={icon} alt={title} className="w-7 mt-3" />
+                <h3 className="my-4 font-semibold text-gray-700">{title}</h3>
+                <p className="text-gray-600 text-sm">{description}</p>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
