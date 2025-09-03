bf4dcba6ff26e441d7a8d94e7c07922639194f86
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-09-04 07:15:00 +0900

    responsive mobile menu

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,7 +3,8 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React, { useState } from "react";
-import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, MoonIcon, SunIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { div } from "framer-motion/client";
 
 const Navbar = () => {
   const theme = "dark";
@@ -25,11 +26,12 @@ const Navbar = () => {
   return (
     <div className="fixed w-full bg-dark/80 backdrop-blur-sm z-50">
       <div className="container max-w-7xl mx-auto px-4">
+        {/* desktop menu */}
         <div className="flex items-center justify-between h-16">
           <Link href="/" className="text-xl font-bold text-primary">
             Berdiyor Orzikulov
           </Link>
-
+          {/* desktop menus */}
           <div className="hidden md:flex items-center space-x-8">
             {menuItems.map((item) => {
               const isActive = pathname === item.href;
@@ -56,7 +58,38 @@ const Navbar = () => {
               )}
             </button>
           </div>
+
+          {/* mobile menu button */}
+          <button
+            onClick={toggleMobileMenu}
+            className="md:hidden p-2 rounded-lg hover:bg-gray-500
+		   dark:hover:bg-gray-800 transition-colors cursor-pointer"
+          >
+            {isMobileMenuOpen ? (
+              <XMarkIcon className="w-6 h-6" />
+            ) : (
+              <Bars3Icon className="w-6 h-6" />
+            )}
+          </button>
         </div>
+
+        {/* mobile menu */}
+        {isMobileMenuOpen && (
+          <div className="md:hidden">
+            <div className="py-4 space-y-4">
+              {menuItems.map((item, index) => (
+                <div key={index}>
+                  <Link
+                    href={item.href}
+                    className="block py-2 hover:text-primary transition-colors"
+                  >
+                    {item.label}
+                  </Link>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
