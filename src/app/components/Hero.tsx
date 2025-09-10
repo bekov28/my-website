cc46fb065941abc79f9807b7d66b23e483801887
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-09-11 01:00:05 +0900

    projects section added

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -46,7 +46,21 @@ const Hero = () => {
           </Link>
         </div>
 
-        
+        <div className="flex flex-col md:flex-row justify-center gap-4">
+          <Link
+            href="/projects"
+            className="bg-primary inline-block w-full md:w-auto text-white px-8 py-3 rounded-lg hover:bg-primary/80 transition-colors"
+          >
+            View Projects
+          </Link>
+
+          <Link
+            href="/contact"
+            className="bg-gray-500 inline-block w-full md:w-auto text-white hover:text-gray-800 px-8 py-3 rounded-lg hover:bg-gray-300 transition-colors"
+          >
+            Contact Me
+          </Link>
+        </div>
       </div>
     </section>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "./components/Hero";
+import Projects from "./components/Projects";
 
 //Home Page
 export default function Home() {
   return (
     <>
       <Hero />
+      <Projects />
     </>
   );
 }
