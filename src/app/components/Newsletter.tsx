d332339281b118079ec652bb536aeb67ca1a1628
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-09-14 09:41:38 +0900

    subscribe newsletter done

--- src/app/components/Newsletter.tsx
+++ src/app/components/Newsletter.tsx
@@ -5,22 +5,31 @@ const Newsletter = () => {
     <section className="py-20 bg-white dark:bg-dark/50 rounded-lg shadow-md overflow-hidden animate-slide-up">
       <div className="p-8 md:p-12">
         <div className="max-w-7xl mx-auto">
-          <div>
-            <h2 className="text-2xl font-bold mb-4">Subscribe to my Newsletter</h2>
-            <p className="text-gray-600 dark:text-gray-300 md:w-2/3">
-              Get the latest updates on my projects, blog posts and tech insights delivered to your
-              inbox.
-            </p>
-          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+            <div>
+              <h2 className="text-2xl font-bold mb-4">Subscribe to my Newsletter</h2>
+              <p className="text-gray-600 dark:text-gray-300 md:w-2/3">
+                Get the latest updates on my projects, blog posts and tech insights delivered to
+                your inbox.
+              </p>
+            </div>
 
-          <form className="flex flex-col md:flex-row gap-4">
-            <input
-              type="email"
-              placeholder="Enter your email"
-              className="flex-1 px-4 py-3 border rounded-lg dark:text-white border-gray-300 dark:border-gray-600 bg-white
-               text-gray-500 dark:bg-dark focus:outline-none focus:ring-2 focus:ring-primary"
-            />
-          </form>
+            <form className="flex flex-col md:flex-row gap-4 items-center">
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="flex-1 px-4 py-3 border rounded-lg dark:text-white border-gray-300 dark:border-gray-600 bg-white
+               text-gray-500 dark:bg-dark focus:outline-none focus:ring-2 focus:ring-primary md:w-auto w-[60%]"
+              />
+              <button
+                type="submit"
+                className="bg-primary text-white px-8 py-3 rounded-lg hover:bg-primary/80 
+                transition-colors md:w-auto w-[60%] inline-block"
+              >
+                Subscribe
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </section>
