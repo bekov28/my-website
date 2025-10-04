8d7826224366ce4748642adb5dfd8f58dabd43b5
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-10-04 18:49:42 +0900

    contact page created

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,5 +1,39 @@
+import { blogs } from "@/contents/blogs";
+import Link from "next/link";
+import { FaCalendar, FaClock } from "react-icons/fa";
+
 const BlogsPage = () => {
-  return <div>Blogs</div>;
+  return (
+    <div className="container max-w-7xl mx-auto py-20">
+      <h1 className="text-4xl font-bold mb-4 text-center">Blogs Posts</h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        {blogs.map((blog) => (
+          <article key={blog.slug} className="bg-white dark:bg-dark/50 rounded-lg shadow-md p-6">
+            <Link href={`/blogs/${blog.slug}`} className="flex flex-col gap-4">
+              <h3 className="text-xl font-semibold mb-2 hover:text-primary transition-colors">
+                {blog.title}
+              </h3>
+            </Link>
+
+            <p className="text-gray-600 dark:text-gray-300 mb-4">{blog.excerpt}</p>
+
+            <div className="flex items-center text-sm text-gray-500 dark:text-gray-400 space-x-4">
+              <span className="flex items-center">
+                <FaCalendar className="mr-2" />
+                {new Date(blog.date).toLocaleDateString()}
+              </span>
+
+              <span className="flex items-center">
+                <FaClock className="mr-2" />
+                {blog.readTime}
+              </span>
+            </div>
+          </article>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default BlogsPage;
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,4 +1,175 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+import { FaEnvelope, FaMapMarkerAlt, FaPhone } from "react-icons/fa";
+
+interface FormData {
+  name: string;
+  email: string;
+  message: string;
+}
+
+type FormStatus = "idle" | "loading" | "success" | "error";
+
 const ContactPage = () => {
-  return <div>Contact Page</div>;
+  const [formData, setFormData] = useState<FormData>({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [status, setStatus] = useState<FormStatus>("idle");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setStatus("loading");
+    try {
+      const response = await fetch("/api/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to send message");
+      }
+
+      setStatus("success");
+      setFormData({
+        name: "",
+        email: "",
+        message: "",
+      });
+    } catch (error) {
+      setStatus("error");
+      console.log("Error sending message", error);
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  return (
+    <div className="container max-w-7xl mx-auto py-20">
+      <h1 className="text-4xl font-bold mb-20 text-center">Contact Me</h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+        {/* contact info */}
+        <div className="space-y-8">
+          <h2 className="text-2xl font-semibold mb-4">Get in Touch</h2>
+          <p className="text-secondary md:w-2/3">
+            I&apos;m always open to discussing new projects, creative ideas, or opportunities to be
+            part of your visions.
+          </p>
+
+          <div className="space-y-4">
+            <div className="flex items-center gap-4">
+              <FaEnvelope className="w-6 h-6 text-primary" />
+              <div>
+                <h3 className="text-semibold">Email</h3>
+                <Link
+                  href="mailto:berdiyororzu@gmail.com"
+                  className="text-secondary hover:text-primary"
+                >
+                  berdiyororzu@gmail.com
+                </Link>
+              </div>
+            </div>
+
+            <div className="flex items-center gap-4">
+              <FaPhone className="w-6 h-6 text-primary" />
+              <div>
+                <h3 className="text-semibold">Phone</h3>
+                <Link href="tel: 010-2811-8826" className="text-secondary hover:text-primary">
+                  +(82) 10-2811-8826
+                </Link>
+              </div>
+            </div>
+
+            <div className="flex items-center gap-4">
+              <FaMapMarkerAlt className="w-6 h-6 text-primary" />
+              <div>
+                <h3 className="text-semibold">Location</h3>
+                <p className="text-secondary">Seoul, South Korea</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* contact form */}
+        <div className="bg-white dark:bg-dark/50 p-6 rounded-lg shadow-md">
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="name" className="block text-sm font-medium mb-2">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                required
+                value={formData.name}
+                onChange={handleChange}
+                placeholder="Enter your name"
+                className="w-full px-4 py-2 rounded-md border border-gray-300 dark:border-gray-700 bg-white dark:bg-dark focus:ring-2
+                 focus:ring-primary focus:border-none"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium mb-2">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+                placeholder="Enter your email"
+                className="w-full px-4 py-2 rounded-md border border-gray-300 dark:border-gray-700 bg-white dark:bg-dark focus:ring-2
+                 focus:ring-primary focus:border-none"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="message" className="block text-sm font-medium mb-2">
+                Message
+              </label>
+              <textarea
+                rows={4}
+                required
+                id="message"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                placeholder="Enter your message"
+                className="w-full px-4 py-2 rounded-md border border-gray-300 dark:border-gray-700 bg-white dark:bg-dark focus:ring-2
+                 focus:ring-primary focus:border-none"
+              />
+            </div>
+
+            <button type="submit" className="w-full btn btn-primary">
+              {status === "loading" ? "Sending..." : "Send Message"}
+            </button>
+
+            {status === "success" && (
+              <p className="text-green-500 text-center">Message sent successfully!</p>
+            )}
+            {status === "error" && (
+              <p className="text-red-500 text-center">Failed to send message. Please try again!</p>
+            )}
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default ContactPage;
