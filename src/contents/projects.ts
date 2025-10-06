a8f5b200369619c401da90fe6e1b4be82dc4e634
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-10-06 23:08:36 +0900

    project description updated

--- src/contents/projects.ts
+++ src/contents/projects.ts
@@ -11,12 +11,21 @@ export const projects: Project[] = [
     image: "/projects/vue.png",
   },
   {
-    title: "Premium Granite Shop",
-    description: `This website is aimed for Web developers to find developer jobs, 
-    especially Vue jobs and for employers to hire web developers. JSON Web Server was used for backend, so actual job contents can not be browsed in the demo version.`,
-    technologies: ["Vue.js", "Pinia", "JSON Server", "Tailwind CSS"],
+    title: "Premium Granite E-commerce Shop",
+    description: `This Firebase-powered full stack website is an online store where users can sign up and look through the all types of valuables stones, such as Granite, Quartz, Marble, etc. Built in Vue.js, user roles and admin role is separated, where only the admin can add and edit the product list.`,
+    technologies: ["Vue.js", "Pinia", "Firebase", "Bootstrap"],
     githubLink: "https://github.com/bekov28/granite-online-shop",
     demoLink: "https://graniteshop-29e1a.web.app/",
     image: "/projects/stone-website.png",
+  },
+  {
+    title: "Campcar Renting Platform",
+    description: `This front-end-only website enables users to rent and buy campcars. 
+    Built in React.js and JavaScript, this website features a user-friendly interface that allows users to rent and buy campcars. 
+    Used tools in this project also include styled-components, MUI library, React Router and React Hooks`,
+    technologies: ["React.js", "MUI", "Styled components", "React Router"],
+    githubLink: "https://github.com/bekov28/camper-project-react",
+    demoLink: "https://campertestsite.netlify.app/",
+    image: "/projects/camper.png",
   },
 ];
