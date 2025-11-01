9208b9336dae9caedf92a4f964be5ff3d6aa4e4b
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-11-01 22:18:29 +0900

    Projects page updated

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -8,7 +8,7 @@ import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
 const Projects = () => {
   return (
     <section className="py-20 container max-w-7xl mx-auto px-4">
-      <h2 className="text-3xl font-bold mb-12 text-center">Featured Projects</h2>
+      <h2 className="text-3xl font-bold mb-12 text-center">Check My Projects</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         {projects.map((project) => (
