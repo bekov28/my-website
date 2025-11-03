468679aae4dd811c2628e1796b5ff695e8589040
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-11-03 23:57:55 +0900

    blog section updated

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -7,26 +7,19 @@ import { pageTransition } from "@/utils/animation";
 const BlogPage = () => {
   return (
     <motion.section className="container max-w-7xl mx-auto py-15" {...pageTransition}>
-      <h1 className="text-4xl font-bold mb-4 text-center">How I became a software developer?</h1>
+      <h1 className="text-4xl font-bold mb-4 text-center">A bit about me...</h1>
       <h2 className="py-15 text-2xl leading-10">
-        Hi, first of all, it is good to meet you in my personal website! Today I wanted to allocate
-        some time to write how I became a software developer without a CS degree! Yes, you read
-        correctly now- I dont have a Computer Science Degree. Hmm, perhaps after reading this you
-        might not have been surprised at all, since nowadays it is common to see software developers
-        without a degree. But, hey, I have to tell that I have a Master Degree in Business (MBA)
-        from Dongseo University. At least I have a degree, right? Now, lets jump straight into
-        today&apos;s topic where I would expain how I became a software developer in South Korea.
-      </h2>
-      <h2 className="text-2xl leading-10">
-        Well, it has not been so long since I started coding. In fact, I started learning coding
-        seriously since 2024 summer from HTML and CSS. Before that I had already been working for
-        two years as a Marketing and Customer Service Specialist in one of the well-known fintech
-        companies in Seoul. In fact, when I worked as a Marketing staff that time, I was not just
-        enjoying the work and simly didnt see any self-development in myself. This was the reason
-        when I decided to enroll an online coding bootcamp. A friend of mine (Uzbek) who was a
-        senior software engineer in a Korean company had opened an online coding course where he was
-        willing to teach coding from scratch. That was the moment that I realized I need to enroll
-        and be really serious about my career.
+        Hello everyone! Glad to see you here in my website. In this very first post, I want to
+        briefly introduce myself. So, my name is Berd and I am originally from Uzbekistan. I came to
+        South Korea in 2015 as a Korean Government Scholarship Program (GKS, formerly KGSP). As part
+        of this scholarship program, I completed a 1-year Korean language course in Inha University
+        and later got my Master&apos;s degree in Dongseo University. Since then, I have worked in
+        numerious small and middle sized Korean companies as a Marketing Manager, Business
+        Developer, Customer Relationship Manager and as an Overseas Sales Assistant. But later at
+        the end of 2023, I realized that I have passion for programming and started to teach myself
+        coding. I also attended a Full Stack Web Developer Bootcamp, where I gained skills in
+        JavaScript, React, TypeScript, Node.js for backend (MERN Stack). Since March 2025, I have
+        been working in Seoul as a Full Stack Developer in a Korean IT company, called coForward.
       </h2>
       <div className="flex justify-center">
         <Image
@@ -37,21 +30,10 @@ const BlogPage = () => {
           className="mt-15 rounded-2xl"
         />
       </div>
-
       <h2 className="py-15 text-2xl leading-10">
-        So, I learned coding from very basics in this IT bootcamp. After finishing HTML and CSS and
-        did some small projects, we then moved to JavaScript where I was simply blown away. It was
-        JavaScript that the real programming language we needed to deal with. First, it didnt seem
-        to be difficult as much as we didnt spend much time on the language and within two weeks we
-        started learning how to code in React.js. We were around 6-7 ambitious students in the class
-        and already started doing a small-medium project in React.js to better understand the JS
-        library (some people confuse that React.js is the framework). But simply, I dont recommend
-        anyone who just started learning JavaScript to jump to frameworks too quickly. I would say
-        spend at least 2-3 months to understand JavaScript fundamentals, do some small projects in
-        vanialla JS and then you might start learning React.js, Vue or other frameworks. So, after
-        finishing the project within 1 month, we moved to TypeScript and then Node.js and MongoDB
-        for backend. And at the end of year, after 6months of study, we finished the course and
-        finally started to apply for jobs (to be continued...).
+        In coForward, I mainly focus on Front-end development, and my main tech stack in Front-end
+        is Vanilla JS, Vue.js and jQuery. Besides front-end, I also do some backend with Java and
+        Spring Boot (both Gradle and Maven tools) and automate the tasks.
       </h2>
     </motion.section>
   );
