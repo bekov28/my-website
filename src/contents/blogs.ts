c1aa15f2d24ef760ee8daf2b6a3d6e78e5413a77
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-12-20 00:37:35 +0900

    new blog post template created

--- src/contents/blogs.ts
+++ src/contents/blogs.ts
@@ -5,7 +5,14 @@ export const blogs: Blog[] = [
     title: "A little about me...",
     excerpt: "Let me introduce myself in this post",
     date: "2025-10-08",
-    readTime: "5 min read",
-    slug: "software-developer",
+    readTime: "3 min read",
+    slug: "about-me",
+  },
+  {
+    title: "30 JavaScript Projects",
+    excerpt: "30 Days, 30 JavaScript Mini Projects...",
+    date: "2025-11-08",
+    readTime: "7 min read",
+    slug: "mini-projects",
   },
 ];
