23510c44144665e96c52dc7f65ad653de91651ec
Bekov28 <orziqulov_berdiyor@mail.ru>
2026-02-06 22:59:48 +0900

    feat: link added for project

--- src/contents/projects.ts
+++ src/contents/projects.ts
@@ -6,10 +6,10 @@ export const projects: Project[] = [
     description: `This Web-only Trading Platform of Shinhan Bank enables customers to register an account and start exchanging varius types of SWAP operations and other trading operations.`,
     technologies: ["Vue.js", "Java", "Spring-boot", "Maven"],
     githubLink: "",
-    demoLink: "",
+    demoLink: "https://efx.shinhan.com/",
     image: "/projects/shinhan.png",
     role: "Role: Front-end developer, worked on the UI/UX part, added JS logic and integrated multiple APIs, implemented the front-end components of Account Widget, Service Information, Swap Point Rate",
-    period: "[2025.06 ~ 2025.12]",
+    period: "[2025.06 ~ 2026.02]",
   },
   {
     title:
