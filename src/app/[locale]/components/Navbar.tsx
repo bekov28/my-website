881f982798a9c31ec8e87ddbb1d8a4cd0674beaa
Bekov28 <orziqulov_berdiyor@mail.ru>
2026-03-06 22:28:00 +0900

    feat: added language toggle button to mobile responsive

--- src/app/[locale]/components/Navbar.tsx
+++ src/app/[locale]/components/Navbar.tsx
@@ -114,6 +114,7 @@ const Navbar = () => {
                     <MoonIcon className="w-5 h-5 mr-2" />
                   )}
                 </button>
+                <LanguageToggle />
               </div>
             </div>
           </div>
